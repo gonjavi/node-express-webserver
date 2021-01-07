894505c7893f244567515de962eda0d10766e27a
manci <javiercaliescali@gmail.com>
2021-01-06 22:55:29 -0500

    crear parciales con hbs

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
  
 app.use(express.static(__dirname + '/public'));
 
 // express hbs
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {  
@@ -13,6 +15,15 @@ app.get('/', (req, res) => {
     year: new Date().getFullYear()
   });
 });
+
+app.get('/about', (req, res) => {  
+  
+  res.render('about', {
+    nombre: 'Gonzalo',
+    year: new Date().getFullYear()
+  });
+});
+
 app.get('/data', (req, res) => {  
   res.send('data');
 });
