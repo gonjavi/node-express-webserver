7fe2cf52332f613859c46e01066f4da605e34a23
manci <javiercaliescali@gmail.com>
2021-01-06 22:08:49 -0500

    instlar hbs, cambio a home.hbs y agregar año y nombre dinamicamente

--- server.js
+++ server.js
@@ -3,13 +3,15 @@ const app = express()
  
 app.use(express.static(__dirname + '/public'));
 
+// express hbs
+app.set('view engine', 'hbs');
+
 app.get('/', (req, res) => {  
-  let salida = {
-    nombre: 'peter',
-    edad: '25',
-    url: req.url
-  }
-  res.send(salida);
+  
+  res.render('home', {
+    nombre: 'Gonzalo',
+    year: new Date().getFullYear()
+  });
 });
 app.get('/data', (req, res) => {  
   res.send('data');
